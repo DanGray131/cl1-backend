429fd4043bdf11b301bc74ae8bbd154e59870519
Emanuel <emanueljlv@gmail.com>
2024-09-23 09:55:35 -0500

    carga inicial

---
+++ src/main/java/pe/edu/cibertec/cl1back/Cl1BackApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/cl1back/Cl1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
